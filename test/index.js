12fede5629590771542e35bf4a4c51df936cd585
Nikolay Turpitko <nikolay@turpitko.com>
2015-04-07 21:22:54 +0600

    Fix usage babel in tests

--- test/index.js
+++ test/index.js
@@ -1,25 +1,9 @@
 import chai from 'chai';
 import xhr from '../index';
-import sinon from 'sinon';
 
 chai.should();
 
-describe('#get', function () {
-    /*
-    beforeEach(function() {
-        this.xhr = sinon.useFakeXMLHttpRequest();
-
-        this.requests = [];
-        this.xhr.onCreate = function(xhr) {
-            this.requests.push(this.xhr);
-        }.bind(this);
-    });
-
-    afterEach(function() {
-        this.xhr.restore();
-    });
-    */
-
+describe('get', function () {
     it('get caching', function () {
         console.log('Test OK');
     });
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+var webpack = require('webpack');
+
+module.exports = {
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+       ]
+    }
+};
