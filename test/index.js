3089cbacf2325a4329fde3c880345497bf9b4eb8
Andrey Ustinov <ustinov.post@gmail.com>
2015-04-08 01:43:13 +0300

    Sample sinon

--- test/index.js
+++ test/index.js
@@ -1,9 +1,21 @@
 import chai from 'chai';
+import sinon from 'sinon';
 import xhr from '../index';
 
 chai.should();
 
 describe('get', function () {
+    beforeEach(function() {
+        this.xhr = sinon.stub();
+        //this.xhr = sinon.useFakeXMLHttpRequest();
+        //var server = sinon.fakeServer.create();
+        //this.xhr = sinon.xhr;
+    });
+
+    afterEach(function(){
+        //this.xhr.restore();
+    });
+
     it('get caching', function () {
         console.log('Test OK');
     });
