423868f5f435a471d4ba3b3fcdc61c85630fe4ac
Nikolay Turpitko <nikolay@turpitko.com>
2015-04-08 12:27:00 +0600

    Add sinon

--- test/index.js
+++ test/index.js
@@ -1,9 +1,24 @@
 import chai from 'chai';
 import xhr from '../index';
+import sinon from 'sinon';
 
 chai.should();
 
 describe('get', function () {
+
+    beforeEach(function () {
+        this._xhr = sinon.useFakeXMLHttpRequest();
+
+        this.requests = [];
+        this._xhr.onCreate = function (xhr) {
+            this.requests.push(xhr);
+        }.bind(this);
+    });
+
+    afterEach(function () {
+        this._xhr.restore();
+    });
+
     it('get caching', function () {
         console.log('Test OK');
     });
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,16 @@ var webpack = require('webpack');
 
 module.exports = {
     module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-       ]
-    }
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }],
+        noParse: [
+            /sinon/
+        ]
+    },
+    plugins: [
+	    new webpack.NormalModuleReplacementPlugin(/sinon/, __dirname + "/node_modules/sinon/pkg/sinon-1.14.1.js")
+    ]
 };
